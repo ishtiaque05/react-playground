c40f8caa7f73722ea2b3d0ce966d99f15d7d4be3
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-11-16 00:36:31 +0600

    Complete signup with validation - added modal for terms and condition - added validation - added loader - correct radio button state

--- src/components/Pages/SignUpForm.js
+++ src/components/Pages/SignUpForm.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Validator from "validator";
 import { Link } from "react-router-dom";
-import { Grid, Button, Form, Icon, Radio, Modal, Divider } from "semantic-ui-react";
+import {
+  Grid,
+  Button,
+  Form,
+  Icon,
+  Radio,
+  Modal,
+  Loader,
+  Dimmer,
+} from "semantic-ui-react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import moment from "moment";
 import "react-day-picker/lib/style.css";
@@ -16,12 +25,12 @@ class SignUpForm extends React.Component {
     data: {
       email: "",
       password: "",
-      dob: null,
+      dob: "",
       firstname: "",
       lastname: "",
+      gender: "",
     },
-    termsVisible: false,
-    errors: {},
+    errors: {}
   };
 
   onChange = e =>
@@ -45,21 +54,26 @@ class SignUpForm extends React.Component {
       isDisabled: modifiers.disabled,
     });
   };
-  handleChange = e =>
-    this.setState({ data: { ...this.state.data, gender: e.target.value } });
+  handleChange = (e, { value }) =>
+    this.setState({ ...this.state.data, gender: value });
 
-  showTerms = () => {
-    this.setState({ termsVisible: true });
-  };
   validate = data => {
     const errors = {};
-    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if (!Validator.isEmail(data.email)) {
+      errors.email = "Invalid email";
+    } else if (!data.email) {
+      errors.email = "Email can't be blank";
+    }
+    if (!data.firstname) errors.firstname = "First name can't be blank";
+    if (!Validator.isISO8601(data.dob.toISOString()))
+      errors.date = "Invalid date";
     if (!data.password) errors.password = "can't be blank";
     return errors;
   };
+  connectTo = () => this.setState({active: true})
 
   render() {
-    const { data, errors, loading, termsVisible } = this.state;
+    const { data, errors, loading, gender } = this.state;
     const formattedDay = data.dob ? moment(data.dob).format(DAY_FORMAT) : "";
 
     const dayPickerProps = {
@@ -72,133 +86,138 @@ class SignUpForm extends React.Component {
 
     return (
       <div className="page-center-signup">
+        <Dimmer active={this.state.active} inverted>
+          <Loader size="large">Loading</Loader>
+        </Dimmer>
         <div className="boxify">
-        <Grid columns={3} centered stackable>
-          <Grid.Row columns={1}>
-            <Grid.Column textAlign="center">
-              <h1>Create a New Account</h1>
-            </Grid.Column>
-            <Grid.Column textAlign="center" className="general-text">
-              Come join the community. Lets us setup your account. Already have
-              an account? <Link to="/login_form">Login</Link>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={4}>
-            <Grid.Column width={4} className="left-align-social-btn">
-              <Grid.Row columns={1}>
-                <Grid.Column>
-                  <p className="general-text">
-                    Why be old fashioned.<br />Just sign up with:
-                  </p>
-                </Grid.Column>
-                <div className="social-btn">
-                <Grid.Column>
-                  <Button size="medium" color="facebook" className="signup-with" >
-                    <Icon name="facebook" /> Facebook
-                  </Button>
-                </Grid.Column>
-                <Grid.Column>
-                  <Button color="google plus" className="signup-with">
-                    <Icon name="google plus" /> Google Plus
-                  </Button>
-                </Grid.Column>
-                </div>
-               </Grid.Row>
-            </Grid.Column>
-            <Grid.Column width={5} tablet={6}>
-              <Form error>
-                <Form.Group widths="equal">
-                  <Form.Input
-                    label="First name"
-                    placeholder="First name"
-                    name="firstname"
-                    id="firstname"
-                    onChange={this.onChange}
-                    value={data.firstname}
-                  />
-                  <Form.Input
-                    label="Last name"
-                    placeholder="Last name"
-                    name="lastname"
-                    id="lastname"
-                    onChange={this.onChange}
-                    value={data.lastname}
-                  />
-                </Form.Group>
-                <Form.Field>
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    value={data.email}
-                    onChange={this.onChange}
-                    placeholder="email"
-                  />
-                </Form.Field>
-                <Form.Field>
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={data.password}
-                    onChange={this.onChange}
-                    placeholder="Enter a password"
-                  />
-                </Form.Field>
-                <Form.Field>
-                  <label htmlFor="dob">Date of birth</label>
-                  <DayPickerInput
-                    value={formattedDay}
-                    onDayChange={this.handleDayChange}
-                    format={DAY_FORMAT}
-                    placeholder={`E.g. ${moment()
-                      .locale("en")
-                      .format(DAY_FORMAT)}`}
-                    dayPickerProps={dayPickerProps}
-                  />
-                </Form.Field>
-                <Form.Field>
-                  <Radio
-                    label="male"
-                    name="radioGroup"
-                    value="male"
-                    checked={this.state.value === "male"}
-                    onChange={this.handleChange}
-                  />
-                  <Radio
-                    label="female"
-                    name="radioGroup"
-                    value="female"
-                    checked={this.state.value === "female"}
-                    onChange={this.handleChange}
-                  />
-                </Form.Field>
-                <Form.Field>
-                  <Button fluid positive>
-                    Sign Up
-                  </Button>
-                  <label htmlFor="termsOfCondition">
-                    By signing up you agree with{" "}
-                    <Modal
-                      trigger={
-                        <Link to="#" onClick={this.showTerms}>
-                          terms of condition
-                        </Link>
-                      }
-                      size="large"
-                      closeIcon
-                      container
-                    >
-                      {termsVisible && <TermsAndConditions />}
-                    </Modal>
-                  </label>
-                </Form.Field>
-              </Form>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          <Grid columns={3} centered stackable>
+            <Grid.Row columns={1}>
+              <Grid.Column textAlign="center">
+                <h1>Create a New Account</h1>
+              </Grid.Column>
+              <Grid.Column textAlign="center" className="general-text">
+                Come join the community. Lets us setup your account. Already
+                have an account? <Link to="/login_form">Login</Link>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={4}>
+              <Grid.Column width={4} className="left-align-social-btn">
+                <Grid.Row columns={1}>
+                  <Grid.Column>
+                    <p className="general-text">
+                      Why be old fashioned.<br />Just sign up with:
+                    </p>
+                  </Grid.Column>
+                  <div className="social-btn">
+                    <Grid.Column>
+                      <Button
+                        size="medium"
+                        color="facebook"
+                        className="signup-with"
+                        onClick={this.connectTo}
+                      >
+                        <Icon name="facebook" /> Facebook
+                      </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Button 
+                        color="google plus" 
+                        onClick={this.connectTo}
+                        className="signup-with"
+                      >
+                        <Icon name="google plus" /> Google Plus
+                      </Button>
+                    </Grid.Column>
+                  </div>
+                </Grid.Row>
+              </Grid.Column>
+              <Grid.Column width={5} tablet={6}>
+                <Form error onSubmit={this.onSubmit} loading={loading}>
+                  <Form.Group widths="equal">
+                    <Form.Input
+                      label="First name"
+                      placeholder="First name"
+                      name="firstname"
+                      id="firstname"
+                      onChange={this.onChange}
+                      value={data.firstname}
+                    />
+                    <Form.Input
+                      label="Last name"
+                      placeholder="Last name"
+                      name="lastname"
+                      id="lastname"
+                      onChange={this.onChange}
+                      value={data.lastname}
+                    />
+                  </Form.Group>
+                  {errors.firstname && <InlineError text={errors.firstname} />}
+                  <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      id="email"
+                      name="email"
+                      value={data.email}
+                      onChange={this.onChange}
+                      placeholder="email"
+                    />
+                    {errors.email && <InlineError text={errors.email} />}
+                  </Form.Field>
+                  <Form.Field>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={data.password}
+                      onChange={this.onChange}
+                      placeholder="Enter a password"
+                    />
+                    {errors.password && <InlineError text={errors.password} />}
+                  </Form.Field>
+                  <Form.Field>
+                    <label htmlFor="dob">Date of birth</label>
+                    <DayPickerInput
+                      value={formattedDay}
+                      onDayChange={this.handleDayChange}
+                      format={DAY_FORMAT}
+                      placeholder={`E.g. ${moment()
+                        .locale("en")
+                        .format(DAY_FORMAT)}`}
+                      dayPickerProps={dayPickerProps}
+                    />
+                    {errors.date && <InlineError text={errors.date} />}
+                  </Form.Field>
+                  <Form.Field>
+                    <Radio
+                      label="male"
+                      name="radioGroup"
+                      value="male"
+                      checked={gender === "male"}
+                      onChange={this.handleChange}
+                    />
+                    <Radio
+                      label="female"
+                      name="radioGroup"
+                      value="female"
+                      checked={gender === "female"}
+                      onChange={this.handleChange}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Button fluid positive>
+                      Sign Up
+                    </Button>
+                    <label htmlFor="termsOfCondition">
+                      By signing up you agree with &nbsp;
+                      <TermsAndConditions />
+                    </label>
+                  </Form.Field>
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </div>
       </div>
     );
--- src/components/messages/TermsAndConditions.js
+++ src/components/messages/TermsAndConditions.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { Modal, Header} from "semantic-ui-react";
+import { Modal, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-const TermsAndConditions = () => (
-    <div>
+class TermsAndConditions extends React.Component {
+  state={
+    open: false
+  }
+  show = () => () => this.setState({ open: true })
+  render(){
+    return(
+      <Modal
+      trigger={
+        <Link to="#" onClick={this.show}>
+          terms of condition
+        </Link>
+      }
+      size="tiny"
+      closeIcon
+    >
       <Header icon="archive" content="Terms And Condtions" />
       <Modal.Content>
         <p>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+          illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+          odit aut fugit, sed quia consequuntur magni dolores eos
         </p>
       </Modal.Content>
-      </div>
-  );
+    </Modal>
+    );
+  }
+}
 
 export default TermsAndConditions;
