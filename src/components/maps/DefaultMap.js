576b9815954ff907aa2ace12988173d198e95b2d
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-11-21 00:57:46 +0600

    added navigator.geolocation present check

--- src/components/maps/DefaultMap.js
+++ src/components/maps/DefaultMap.js
@@ -1,34 +1,41 @@
 import React from "react";
 import CurrentLocation from "../maps/CurrentLocation";
 
-class DefaultMap extends React.Component{
-  state={
+class DefaultMap extends React.Component {
+  state = {
     lat: null,
     long: null,
-    error:""
-  }
+    error: "",
+  };
 
-  async componentWillMount(){
+  async componentWillMount() {
     try {
-      const position = await CurrentLocation({ enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 });
-      console.log("positionnnnnnnnn------------", position.coords)
-      this.setState({ lat: position.coords.latitude, long: position.coords.longitude })
-      // do something with position.     
-    }
-    catch (error) {
-      console.log(error)
+      if (navigator.geolocation) {
+        const position = await CurrentLocation({
+          enableHighAccuracy: false,
+          timeout: 20000,
+          maximumAge: 1000,
+        });
+        console.log("positionnnnnnnnn------------", position.coords);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      } else {
+        this.setState({ error: "Browser doesn't support GeoLocation!" });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
-  InitialLocation = (position) =>{
-    this.setState({lat: position.lat, lng: position.long })
-  }
-  render(){
-    
-    const {lat, long, error} = this.state;
-     console.log("data received:",lat);
-    return(
+  InitialLocation = position => {
+    this.setState({ lat: position.lat, lng: position.long });
+  };
+  render() {
+    const { lat, long, error } = this.state;
+    console.log("data received:", lat);
+    return (
       <div>
-
         <h1>{lat}</h1>
         <h1>{long}</h1>
         {error && <h1>Error: {error}</h1>}
