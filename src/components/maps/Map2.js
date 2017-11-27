ec7cd7656eb942081e68a939d98fc1519f10e535
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-11-28 00:19:56 +0600

    Added tracking map - re-rendering issue of google map taken care off with Tracker component - polyline used to draw on map

--- src/components/maps/Map2.js
+++ src/components/maps/Map2.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React from "react";
+import Tracker from "./Tracker";
+const { compose } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+} = require("react-google-maps");
 
-const Map2 = () => <h1>Map two</h1>
+class Map2 extends React.Component {
+  state = {
+    lat: 23.754837,
+    lng: 90.3609216,
+    originlat: 23.7548371,
+    originlng:  90.3609216,
+    lineCoords: [],
+  };
 
-export default Map2
+  render() {
+    const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => (
+      <GoogleMap defaultZoom={8} defaultCenter={{ lat: this.state.originlat , lng: this.state.originlng }}>
+        <Tracker lat={this.state.originlat} lng={this.state.originlng}/>
+      </GoogleMap>
+    ));
+    return (
+      <div>
+        <MapWithAMarker
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default Map2;
---
+++ src/components/maps/Tracker.js
@@ -0,0 +1,66 @@
+/* global google */
+import React from "react";
+import PropTypes from 'prop-types';
+import { Polyline } from "react-google-maps/lib/components/Polyline";
+
+const PubNub = require("pubnub");
+const {
+  Marker
+} = require("react-google-maps");
+
+const pnChannel = "map-demo";
+const pubnub = new PubNub({
+  publishKey: "pub-c-27b4e205-2ce2-4a96-acf1-e374d1610327",
+  subscribeKey: "sub-c-6e2a9480-d2c8-11e7-aee1-6e8e9d2d00b1",
+});
+
+
+class Tracker extends React.Component{
+  state={
+    lat: this.props.lat,
+    lng: this.props.lng,
+    lineCoords: []
+  }
+
+  componentDidMount(){
+    pubnub.subscribe({ channels: [pnChannel] });
+    pubnub.addListener({ message: this.redraw });
+    console.log(pubnub);
+    setInterval(
+      () =>
+        pubnub.publish({
+          channel: pnChannel,
+          message: { lat: this.state.lat + 0.001, lng: this.state.lng + 0.01 },
+        }),
+      1000,
+    );
+  }
+
+  redraw = payload => {
+    console.log("payload: ", payload);
+    this.setState({
+      lat: payload.message.lat,
+      lng: payload.message.lng,
+      lineCoords: [
+        ...this.state.lineCoords,
+        new google.maps.LatLng(payload.message.lat, payload.message.lng),
+      ],
+    });
+  };
+
+  render(){
+    return(
+    <div>
+      <Marker position={{ lat: this.state.lat , lng: this.state.lng }} />
+      <Polyline path={this.state.lineCoords} geodesic strokeColor='#2E10FF' />
+    </div>
+  );
+  }
+}
+
+Tracker.propTypes = {
+  lat: PropTypes.number.isRequired,
+  lng: PropTypes.number.isRequired
+};
+
+export default Tracker;
